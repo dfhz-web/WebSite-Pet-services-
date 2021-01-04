382995bf463548bfce3f0f41c2349e20c33649fd
affai <fh663993@gmail.com>
2021-01-03 22:34:02 -0500

    think has a error

---
+++ database/migrations/2021_01_04_025253_create_assistances_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssistancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+     {
+        Schema::create('assistances', function (Blueprint $table) {
+            $table->id();
+
+            $table->text('symptoms');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->text('address');
+
+            $table->unsignedBigInteger('lesson_id');
+            $table->foreign('lesson_id')->references('id')->on('lessons')->onDelete('cascade');
+
+
+            $table->timestamps();
+        });
+     }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assistances');
+    }
+}
