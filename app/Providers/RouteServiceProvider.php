a8c3c1c8d44e7c6ae5b4cfec661f514e1ed1082f
affai <fh663993@gmail.com>
2021-01-04 22:01:19 -0500

    starting with parts of the coordinationspecially

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -51,6 +51,11 @@ class RouteServiceProvider extends ServiceProvider
                 ->prefix('admin')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/admin.php'));
+
+            Route::middleware('web','auth')
+                ->prefix('coordinator&specially')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/coordinator&specially.php'));
         });
     }
 
---
+++ routes/coordinator&specially.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('Modules', function () {
+    return "i am excited it is my first day as  an amazing worker";
+    
+});
+
+
+
+Route::get('Cordinating', function () {
+    return "Here i am going to coordinate all cases";
+    
+});
